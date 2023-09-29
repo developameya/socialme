f72152e2bc26252cda978fe15f7324918d5e45a1
Ameya Bhagat <developameya@outlook.com>
2023-09-29 15:01:45 +0530

    add app bar for post page.

---
+++ lib/src/features/newsfeed/presentation/widgets/post_page_app_bar_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:socialme/src/features/newsfeed/presentation/pages/edit_post_page.dart';
+
+AppBar buildPostPageAppBar({
+  String? title,
+  required BuildContext context,
+  required bool isAuthor,
+}) {
+  return AppBar(
+    title: Text(
+      title ?? AppLocalizations.of(context)!.defaultPostTitle,
+    ),
+    actions: [
+      if (isAuthor)
+        TextButton(
+          onPressed: () =>
+              Navigator.of(context).pushNamed(EditPostPage.routeName),
+          style: TextButton.styleFrom(foregroundColor: Colors.white),
+          child: Text(
+            AppLocalizations.of(context)!.editButtonLabel,
+          ),
+        ),
+    ],
+  );
+}
