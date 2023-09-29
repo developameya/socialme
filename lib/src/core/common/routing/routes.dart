01758ac646d42e4639d95d1ef16cc4319dbaf9b5
Ameya Bhagat <developameya@outlook.com>
2023-09-29 21:13:42 +0530

    add routes for edit post page and Post page.

--- lib/src/core/common/routing/routes.dart
+++ lib/src/core/common/routing/routes.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:socialme/src/core/common/settings/settings_view.dart';
 import 'package:socialme/src/features/authentication/presentation/pages/sign_in_page.dart';
 import 'package:socialme/src/features/authentication/presentation/pages/sign_up_page.dart';
+import 'package:socialme/src/features/posts/presentation/edit_post_page.dart';
 import 'package:socialme/src/features/newsfeed/presentation/pages/news_feed_page.dart';
-import 'package:socialme/src/features/newsfeed/presentation/pages/sample_item_details_view.dart';
+import 'package:socialme/src/features/newsfeed/presentation/pages/post_page.dart';
+import 'package:socialme/src/features/newsfeed/service/entity/news_feed_item_entity.dart';
 
 Route<dynamic>? routes(RouteSettings routeSettings) => MaterialPageRoute<void>(
       settings: routeSettings,
@@ -15,12 +17,24 @@ Route<dynamic>? routes(RouteSettings routeSettings) => MaterialPageRoute<void>(
             return const SignUpPage();
           case SettingsView.routeName:
             return SettingsView();
-          case SampleItemDetailsView.routeName:
-            return const SampleItemDetailsView();
           case NewsFeedPage.routeName:
             return NewsFeedPage();
+          case EditPostPage.routeName:
+            return const EditPostPage();
+          case PostPage.routeName:
+            //TODO: temp
+            return const PostPage(
+              post: NewsFeedItemEntity(
+                id: "1",
+                author: "author",
+                createdAt: "11/11/11",
+                title: null,
+                description: null,
+              ),
+            );
+
           default:
-            return NewsFeedPage();
+            return const SignInPage();
         }
       },
     );
