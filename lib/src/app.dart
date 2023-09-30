7e7e7d1a7435bfc52609abf425659d451e73cd7f
Ameya Bhagat <developameya@outlook.com>
2023-09-30 14:20:01 +0530

    add logic to navigate based on auth provider.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,39 +1,48 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:socialme/src/core/common/routing/routes.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:socialme/src/core/common/routing/route_delegates.dart';
 import 'package:socialme/src/core/common/services/service_locator.dart';
 import 'package:socialme/src/core/common/settings/settings_controller.dart';
+import 'package:socialme/src/dummy_auth_provider.dart';
 
-class SocialMeApp extends StatelessWidget {
+class SocialMeApp extends ConsumerWidget {
   const SocialMeApp({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: sl.get<SettingsController>(),
-      builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          restorationScopeId: 'app',
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', ''), // English, no country code
-          ],
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: sl.get<SettingsController>().themeMode,
-          onGenerateRoute: routes,
-        );
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(dummyAuthProvider);
+
+    return BeamerProvider(
+      routerDelegate: routeDelegate,
+      child: AnimatedBuilder(
+        animation: sl.get<SettingsController>(),
+        builder: (BuildContext context, Widget? child) {
+          return MaterialApp.router(
+            restorationScopeId: 'app',
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en', ''), // English, no country code
+            ],
+            onGenerateTitle: (BuildContext context) =>
+                AppLocalizations.of(context)!.appTitle,
+            theme: ThemeData(),
+            darkTheme: ThemeData.dark(),
+            themeMode: sl.get<SettingsController>().themeMode,
+            routeInformationParser: BeamerParser(),
+            routerDelegate: routeDelegate,
+          );
+        },
+      ),
     );
   }
 }
