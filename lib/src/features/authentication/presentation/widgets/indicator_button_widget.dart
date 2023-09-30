ad63905016cfd94d3df3f2ff0be1c4ede610ff08
Ameya Bhagat <developameya@outlook.com>
2023-09-30 14:18:16 +0530

    update on pressed method of indicator button widget.

--- lib/src/features/authentication/presentation/widgets/indicator_button_widget.dart
+++ lib/src/features/authentication/presentation/widgets/indicator_button_widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:socialme/src/core/common/widgets/progress_indicator_widget.dart';
+import 'package:socialme/src/dummy_auth_provider.dart';
 
-class IndicatorButtonWidget extends HookWidget {
+class IndicatorButtonWidget extends HookConsumerWidget {
   final String label;
 
   ///A button with loading indicator.
@@ -13,12 +15,13 @@ class IndicatorButtonWidget extends HookWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final isLoading = useState(false);
     return ElevatedButton(
       onPressed: () {
         //add logic
         isLoading.value = !isLoading.value;
+        ref.read(dummyAuthProvider.notifier).state = true;
       },
       child: SizedBox(
         width: 100,
