dcc32dc833818f98853579ca3ac914855234b335
Ameya Bhagat <developameya@outlook.com>
2023-09-30 14:18:52 +0530

    update routing logic in authlocation

--- lib/src/features/authentication/location.dart
+++ lib/src/features/authentication/location.dart
@@ -3,22 +3,22 @@ import 'package:flutter/widgets.dart';
 import 'package:socialme/src/features/authentication/presentation/pages/sign_in_page.dart';
 import 'package:socialme/src/features/authentication/presentation/pages/sign_up_page.dart';
 
-///A location which provides routes for all the pages within authentication.
+///Build a stack of pages related to authentication.
+///
 ///
 class AuthLocation extends BeamLocation<BeamState> {
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
     return [
-      if (state.uri.toString() == SignInPage.routeName)
-        const BeamPage(
-            key: ValueKey(SignInPage.routeName),
-            title: "socialme! - signin",
-            child: SignInPage()),
+      const BeamPage(
+          key: ValueKey(SignInPage.routeName),
+          title: "socialme! - signin",
+          child: SignInPage()),
       if (state.uri.toString() == SignUpPage.routeName)
         const BeamPage(
             key: ValueKey(SignUpPage.routeName),
             title: "socialme! - signup",
-            child: SignInPage()),
+            child: SignUpPage()),
     ];
   }
 
